a6b039835a0c8736405e6c5d2c61f0a35e3fa8a7
Danil12121 <95086085+Danil12121@users.noreply.github.com>
2024-11-08 17:34:20 +0300

    Update area.go

--- calculator/area.go
+++ calculator/area.go
@@ -7,7 +7,7 @@ import (
 func TotalArea(elems []shapes.Shape) float64 {
 	res := 0.0
 	for _, elem := range elems {
-		sq := shapes.CallSquare(elem)
+		sq := elem.Area()
 		res += sq
 	}
 	return res

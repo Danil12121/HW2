30a6205af0d4df57f2906efc94e1d314c8b12b90
Danil12121 <95086085+Danil12121@users.noreply.github.com>
2024-11-08 17:33:01 +0300

    Update shapes.go

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,13 +1,11 @@
 package shapes
 
 import (
-	"errors"
 	"math"
-	"reflect"
 )
 
 type Shape interface {
-	Square() float64
+	Area() float64
 }
 type Circle struct {
 	radius float64
@@ -17,36 +15,18 @@ type Rectangle struct {
 	length, width float64
 }
 
-func CreateCircle(radius any) (Circle, error) {
-	var m float64
-	if reflect.TypeOf(radius) != reflect.TypeOf(m) {
-		return Circle{}, errors.New("radius must be float64")
-	}
-	m = radius.(float64)
-	return Circle{radius: m}, nil
+func CreateCircle(radius float64) Circle {
+	return Circle{radius: radius}
 }
 
-func CreateRectangle(width, length any) (Rectangle, error) {
-	var m, n float64
-	if reflect.TypeOf(width) != reflect.TypeOf(m) {
-		return Rectangle{}, errors.New("width must be float64")
-	}
-	if reflect.TypeOf(length) != reflect.TypeOf(n) {
-		return Rectangle{}, errors.New("length must be float64")
-	}
-	m = width.(float64)
-	n = length.(float64)
-	return Rectangle{width: m, length: n}, nil
+func CreateRectangle(width, length float64) Rectangle {
+	return Rectangle{width: width, length: length}
 }
 
-func (c Circle) Square() float64 {
+func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
-func (r Rectangle) Square() float64 {
+func (r Rectangle) Area() float64 {
 	return r.width * r.length
-}
-
-func CallSquare(s Shape) float64 {
-	return s.Square()
 }
